e4478167e9a1330704eff5b93a3ca87c341cd585
David Vick <CoolMan3428@yahoo.com>
2012-03-14 21:49:24 -0500

    first commit

---
+++ src/me/Irock23/PlayStoreOpener/main.java
@@ -0,0 +1,35 @@
+package me.Irock23.PlayStoreOpener;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.main);
+    	Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.vending");
+    	if(isCallable(intent)) {
+        	startActivity(intent);
+    	} else {
+    		MsgBox("Google Play Store not found!");
+    	}
+    	finish();
+    }
+    
+    private boolean isCallable(Intent intent) {  
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);  
+        return list.size() > 0;  
+    }
+    
+    public void MsgBox(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
